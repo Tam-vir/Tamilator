796951bedd7e56080777727c631747c0672dcea8
Tam-vir <astronomertamvir2020@gmail.com>
2026-04-22 14:59:23 +0600

    cleaning up

--- include/SDL2/SDL_revision.h
+++
@@ -1,8 +0,0 @@
-/* #undef SDL2COMPAT_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
-
-#ifdef SDL2COMPAT_VENDOR_INFO
-#define SDL_REVISION "SDL-release-2.32.66-0-g2b38b37 (" SDL2COMPAT_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-release-2.32.66-0-g2b38b37"
-#endif
